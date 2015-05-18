84a26fb2aab2d536e84224271719915a55f66831
Francisco Rangel <frankjrangel@gmail.com>
2015-05-17 21:40:08 -0400

    Construccion template

--- single.php
+++ single.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
-
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php the_title(); ?>
   <?php endwhile; else: ?>
     <p>Nada que mostrar</p>
   <?php endif; ?>
-
 <?php get_footer(); ?>
