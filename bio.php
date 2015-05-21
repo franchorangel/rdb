b2f6fc441f2da46b8955131cd235b63e9becc82a
Francisco Rangel <frankjrangel@gmail.com>
2015-05-20 20:23:38 -0430

    Arreglando bio

--- bio.php
+++ bio.php
@@ -1,8 +1,10 @@
 <?php //Template Name: Bio ?>
 <?php get_header(); ?>
+<div class="main">
 <div id="contenido-bio">
 <?php if( have_posts() ) : while ( have_posts() ) : the_post() ?>
 <?php the_content(); ?>
 <?php endwhile; endif; ?>
+</div>
 </div>
 <?php get_footer(); ?>
