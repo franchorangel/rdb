63ac3b90c66f7c0c0fa4b30e795c1da6156e8011
Francisco Rangel <frankjrangel@gmail.com>
2015-05-25 23:19:49 -0400

    Estructurando single

---
+++ single-video.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="main">
+<div id="fill"></div>
+<div id="contenido">
+<?php if( have_posts() ) : while ( have_posts() ) : the_post() ?>
+<?php the_content(); ?>
+<?php endwhile; endif; ?>
+</div>
+</div>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,7 +1,15 @@
 <?php get_header(); ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <?php the_title(); ?>
+<div class="main">
+<div id="fill"></div>
+<div id="contenido">  
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <h3><?php //get date ?></h3>
+    <?php //get featured image ?>
+    <p><?php the_content(); ?></p>
   <?php endwhile; else: ?>
     <p>Nada que mostrar</p>
   <?php endif; ?>
+</div>
+</div>
 <?php get_footer(); ?>
