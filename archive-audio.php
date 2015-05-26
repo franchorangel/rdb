7b7e9d38ca71f5c78ab3e095219e70547dcded22
Francisco Rangel <frankjrangel@gmail.com>
2015-05-25 23:31:17 -0400

    Estructurando archives

--- archive-audio.php
+++ archive-audio.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
-
-Archive de audio
-
+<!-- Agregar los estilos en el menu para denotar la seccion -->
+<div class="main">
+<div id="fill"></div>
+<div id="contenido">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="archive-item">
+      <h1><?php the_title(); ?></h1>
+      <?php // get sneak peak ?>
+    </div>
+  <?php endwhile; else: ?>
+    <p>Nada que mostrar</p>
+  <?php endif; ?>
+</div>
+</div>
 <?php get_footer(); ?>
