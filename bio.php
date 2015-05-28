42e2da15237ef5db85fe9c62fe60d15fd06e2d28
Francisco Rangel <frankjrangel@gmail.com>
2015-05-27 23:35:21 -0400

    Arreglo wrapper

--- bio.php
+++ bio.php
@@ -2,7 +2,7 @@
 <?php get_header(); ?>
 <div class="main">
 <div id="fill"></div>
-<div id="contenido-bio">
+<div id="contenido">
 <?php if( have_posts() ) : while ( have_posts() ) : the_post() ?>
 <?php the_content(); ?>
 <?php endwhile; endif; ?>
