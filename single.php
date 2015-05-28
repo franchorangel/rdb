546924f22feb98911a8325dbe360662576173a86
Francisco Rangel <frankjrangel@gmail.com>
2015-05-27 23:55:30 -0400

    Fecha y autor listo en single, arreglos menores

--- single.php
+++ single.php
@@ -4,8 +4,7 @@
 <div id="contenido">  
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <h1><?php the_title(); ?></h1>
-    <h3><?php //get date ?></h3>
-    <?php //get featured image ?>
+    <h3><?php echo get_the_date(); ?> <span> - </span> <?php echo get_the_author(); ?></h3>
     <p><?php the_content(); ?></p>
   <?php endwhile; else: ?>
     <p>Nada que mostrar</p>
