5b982ad00894ab94f36f22fe6d7150234bbae26b
Francisco Rangel <frankjrangel@gmail.com>
2015-08-13 12:50:46 -0430

    Fix video home

--- front-page.php
+++ front-page.php
@@ -6,8 +6,10 @@
     <?php endif; ?>
     <div id="home-video-fill">
     </div>
-    <div id="home-video-wrapper">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/OAZNJWdis8E??iv_load_policy=3;rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+    <div id="video-reg">
+        <div id="home-video-wrapper">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/OAZNJWdis8E??iv_load_policy=3;rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+        </div>
     </div>
   </div>
   <div id="presentaciones">
